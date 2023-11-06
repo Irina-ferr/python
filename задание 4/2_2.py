b7ffd3bd4df859203080b02c2a06adadf66de720
Irina <vega_is@mail.ru>
2023-11-06 22:02:42 +0400

    добавлены сделанные задачи

---
+++ задание 4/2_2.py
@@ -0,0 +1,10 @@
+#Y или (X и не Y или Z)
+print ('  x  ', '  y  ', '  z  ', 'значение')
+
+#сделать чтобы печатало табличку истинности ?
+for x in (False,True):
+    for y in (False,True):
+        for z in (False,True):
+            t = y or (x and (not y or z))
+            print (x, y, z, '   ', t)
+
