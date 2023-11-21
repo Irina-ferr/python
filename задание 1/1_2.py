109818709222fb32dfdd6892964c5ab906e8d3d6
Ferafontova Irina <130399220+Irina-ferr@users.noreply.github.com>
2023-11-21 13:10:22 +0400

    Add files via upload

---
+++ задание 1/1_2.py
@@ -0,0 +1,4 @@
+surname = input ('введите фамилию ')
+name = input ('введите имя ')
+patronymic = input ('введите отчество ')
+print (str(name) +' '+ str(patronymic) +' '+ str(surname))
---
+++ задание 1/1_4.py
@@ -0,0 +1,8 @@
+day = input ('введите день ')
+month = input ('введите месяц ')
+year = input ('введите год ')
+print (str(day) + '.' + str(month) + '.' + str(year))
+print (str(day) + '/' + str(month) + '/' + str(year))
+print (str(day) + ' ' + str(month) + ' ' + str(year))
+print (str(day) + '-' + str(month) + '-' + str(year))
+
