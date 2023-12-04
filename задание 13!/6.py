686868b77798382a22c860cf4955b6c41f4c3f51
Ferafontova Irina <130399220+Irina-ferr@users.noreply.github.com>
2023-12-05 00:05:07 +0400

    Add files via upload

--- задание 13!/6.py
+++ задание 13!/6.py
@@ -1,1 +1,12 @@
 # e^−t * cos(2πt) на отрезке t ∈ [−10; 10] с шагом 1 и с шагом 0.25;
+import numpy as np
+dt = 1
+t = np. arange ( -10 , 10+ dt , dt )
+n=np.e**(-t)*np.cos(2*np.pi*t)
+for i in range ( len ( t )):
+    print ( t [ i ] , n [ i ])
+dt = 0.25
+t = np. arange ( -10 , 10+ dt , dt )
+n=np.e**(-t)*np.cos(2*np.pi*t)
+for i in range ( len ( t )):
+    print ( t [ i ] , n [ i ])
