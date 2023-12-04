bcb391e2a32a9ca352605166466f6bb5f782367d
Ferafontova Irina <130399220+Irina-ferr@users.noreply.github.com>
2023-12-04 10:18:16 +0000

    сделано

--- задание 13/1.py
+++ задание 13/1.py
@@ -1,1 +1,18 @@
 # x^2 на отрезке x ∈ [−2; 2] с шагом 0.01, с шагом 0.1, с шагом 0.25;
+# пример
+import numpy as np
+dt = 0.01
+x = np. arange ( -2 , 2+ dt , dt )
+n = np. square  ( x )
+for i in range ( len ( x )):
+    print ( x [ i ] , n [ i ])
+dt = 0.1
+x = np. arange ( -2 , 2+ dt , dt )
+n = np. square  ( x )
+for i in range ( len ( x )):
+    print ( x [ i ] , n [ i ])
+dt = 0.25
+x = np. arange ( -2 , 2+ dt , dt )
+n = np. square  ( x )
+for i in range ( len ( x )):
+    print ( x [ i ] , n [ i ])
